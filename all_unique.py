76ca5678d9d914f4c7066c50cefd85df31168af3
Alex <ayazdani@usc.edu>
2025-07-09 14:15:32 -0700

    added all_unique

---
+++ all_unique.py
@@ -0,0 +1,25 @@
+r'''
+Alex Yazdani
+9 July 2025
+
+word_pattern.py
+Write a function that returns True if all characters in a string are unique (no repeats), False otherwise.
+'''
+
+
+def all_unique(str):
+    chars = {}
+    for c in str:
+        if c in chars:
+            return False
+        chars[c] = True
+    return True
+
+
+if __name__ == "__main__":
+    print(all_unique("abcdef"))     # True
+    print(all_unique("hello"))      # False
+    print(all_unique(""))           # True
+    print(all_unique("AaBbCc"))     # True
+    print(all_unique("Python!"))    # True
+    print(all_unique("112233"))     # False
